3536a37f4940b27148154faf7f007db1df35e40a
DFLyan <jiweichen1994@163.com>
2023-12-21 15:33:17 +0800

    Create downsample_depth_or_semantic_label.py

---
+++ mmdetection/downsample_depth_or_semantic_label.py
@@ -0,0 +1,9 @@
+downsample = 16
+se = box_2d_mask_aug[4]
+B, N, H, W = se.shape
+se_down = se.view(H // downsample, downsample,
+            W // downsample, downsample, 1)
+se_down = se_down.permute(0, 2, 4, 1, 3).contiguous()
+se_down = se_down.view(-1, downsample*downsample)
+se_down = torch.max(se_down, dim=-1).values
+se_down = se_down.view(H // downsample, W // downsample)
